12f4fc66cf3caf3d9ee792ed54c31e968c00d605
natnael <natiyeshi16@.com>
2022-06-01 13:54:14 +0300

    first update

---
+++ js.js
@@ -0,0 +1,17 @@
+
+
+
+  let map = new Map ([
+    ["img_m1","photo/avatar1.jpg"],
+    ["img_m2","photo/avatar2.jpg"],
+    ["img_m3","photo/avatar1.jpg"]
+    ]);             
+
+    console.log(map.get("img_m1"));
+
+
+    const fruits = new Map([
+        ["apples", 500],
+        ["bananas", 300],
+        ["oranges", 200]
+      ]);
